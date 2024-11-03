e49479f8fef84b3db8b692e7793258a67bc51cc3
unknown <chiheb.design@gmail.com>
2024-11-03 22:11:15 +0100

    add icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxthub/core"],
-
   runtimeConfig: {
     anthropicKey: process.env.ANTHROPIC_KEY,
   },
   hub: {
-    database: true
-  }
+    database: true,
+  },
 });
